3a97cb4a04ea49c0dcf89d6d097429550449b349
THEERAWUT <tae57150@gmail.com>
2025-02-17 01:18:36 +0700

    s

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/LInk-Bio.github.io/",
   plugins: [vue()],
   // server: {
   //   // allowedHosts: ["kr39t8-5173.csb.app"], // เพิ่มโฮสต์ที่อนุญาต
   //   port: 8899, 
   //   strictPort: true,
   // },
-  base: "/LInk-Bio.github.io/",
   build: {
     rollupOptions: {
       input: "index.html", // ✅ ระบุ entry file ที่ถูกต้อง
